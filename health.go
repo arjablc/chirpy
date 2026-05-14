bcc046d6170a7ce9b7968e2d285f566333682c12
knot <arjablc@gmail.com>
2026-05-14 23:23:02 +0545

    config: db setup

--- health.go
+++ health.go
@@ -1,10 +1,79 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"unicode/utf8"
+)
+
+var profanes = map[string]string{"kerfuffle": "****", "sharbert": "****", "fornax": "****"}
 
 func hanldeHealthStatus(res http.ResponseWriter, req *http.Request) {
 	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(200)
 	responseBody := []byte("OK")
 	res.Write(responseBody)
+}
+
+func vallidateChirp(res http.ResponseWriter, req *http.Request) {
+	type incoming struct {
+		Body string `json:"body"`
+	}
+	type validRes struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+	reqBody := incoming{}
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Fatalf("Body read error")
+		_errorResponse(res, "Internal Error", 500)
+		return
+	}
+	err = json.Unmarshal(body, &reqBody)
+	if err != nil {
+		log.Fatalf("Unmarshal error")
+		_errorResponse(res, "Internal Error", 500)
+		return
+	}
+	if utf8.RuneCountInString(reqBody.Body) > 140 {
+		_errorResponse(res, "Chirp is too long", 400)
+		return
+	}
+	cleanedBody := _cleanBody(reqBody.Body)
+	res.WriteHeader(200)
+	responseBody := validRes{CleanedBody: cleanedBody}
+	response, err := json.Marshal(responseBody)
+	if err != nil {
+		log.Fatalf("Body marshal error")
+	}
+	res.Write(response)
+	return
+}
+func _cleanBody(body string) string {
+	words := strings.Split(body, " ")
+	for index, word := range words {
+		lowerWord := strings.ToLower(word)
+		if val, ok := profanes[lowerWord]; ok {
+			words[index] = val
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func _errorResponse(res http.ResponseWriter, errorString string, errorCode int) {
+	type errorRes struct {
+		Error string `json:"error"`
+	}
+	res.WriteHeader(errorCode)
+	responseBody := errorRes{Error: "Chirp is too long"}
+	response, err := json.Marshal(responseBody)
+	if err != nil {
+		log.Fatalf("Body marshal error")
+	}
+	res.Write(response)
+	return
+
 }
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/arjablc/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func (config *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
@@ -22,8 +30,13 @@ func (config *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
 
 func (config *apiConfig) metricHandler(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(200)
-	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	data := []byte(fmt.Sprintf("Hits: %d", config.fileserverHits.Load()))
+	res.Header().Add("Content-Type", "text/html; charset=utf-8")
+	data := []byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, config.fileserverHits.Load()))
 	res.Write(data)
 }
 func (config *apiConfig) resetHanlder(res http.ResponseWriter, req *http.Request) {
@@ -32,15 +45,26 @@ func (config *apiConfig) resetHanlder(res http.ResponseWriter, req *http.Request
 }
 
 func main() {
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatalf("Failed to open db")
+	}
+	dbQueries := database.New(db)
 	servMux := http.ServeMux{}
-	cfg := apiConfig{}
+	cfg := apiConfig{db: dbQueries}
 	port := 8080
 	server := http.Server{Handler: &servMux, Addr: ":8080"}
 	fsHandler := http.FileServer(http.Dir("."))
+	//NOTE: app route
 	servMux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricInc(fsHandler)))
-	servMux.HandleFunc("GET /healthz", hanldeHealthStatus)
-	servMux.HandleFunc("GET /metrics", cfg.metricHandler)
-	servMux.HandleFunc("POST /reset", cfg.resetHanlder)
+	//NOTE: api routes
+	servMux.HandleFunc("GET /api/healthz", hanldeHealthStatus)
+	servMux.HandleFunc("POST /api/validate_chirp", vallidateChirp)
+	//NOTE: admin route
+	servMux.HandleFunc("POST /admin/reset", cfg.resetHanlder)
+	servMux.HandleFunc("GET /admin/metrics", cfg.metricHandler)
 	fmt.Println("Serving and listening on port", port)
 	server.ListenAndServe()
 }
